52dc8907769126719dcaada9b7214a56136f701a
Alex <ongaroalex0@gmail.com>
2023-06-07 02:02:50 -0700

    added my git pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+// import Weather from './Weather';
+import './App.css';
+import React from 'react';
+import { useState  } from 'react';
+
+
+const API_KEY = '1ded49cf4768dfef68a1742fb1367614'
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 function App() {
+  // const [location,setLocation] = useState('')
+  const [post, setPost] = useState(null);
+  const [text,setText] = useState("")
+
+
+   
+  const ChangeText=(e)=>{
+  setText(e.target.value)
+  }
+  function ChangeCity(){
+  fetch(`${BASE_URL}weather?q=${text}&appid=${API_KEY}&units=metric`)
+  .then(res => res.json())
+  .then(res =>{
+    setPost(res)
+    console.log(res)
+  }) 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header>
+        <h1>Weather Application</h1>
+    </header>
+    <main>
+        <input type='text' onChange={ChangeText} value={text}/>
+        <button id="search" onClick={ChangeCity}>Search</button>
+        <div id="weather">
+        {post && (
+        <div className="">
+          <h2 className="">{post.name}</h2>
+          <p>Temperature: {post.main.temp}°C</p>
+          <p>Description: {post.weather[0].description}</p>
+          <p>Feels like: {post.main.feels_like}°C</p>
+          <p>Humidity: {post.main.humidity}%</p>
+        </div>
+      )}
+        </div>
+    </main>
+    <footer>
+        <p>Copyright © 2023</p>
+    </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+
+function Weather(){
+    return(
+    //     <div>
+    // <header>
+    //     <h1>Weather Application</h1>
+    // </header>
+    // <main>
+    //     <input type="text" id="location"/>
+    //     <button id="search">Search</button>
+    //     <div id="weather">{post.weather}</div>
+    // </main>
+    // <footer>
+    //     <p>Copyright © 2023</p>
+    // </footer>
+    //     </div>
+    )
+}
+export default Weather
